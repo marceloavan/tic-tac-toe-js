87451d3b1c80d0200f9bb03acfdde0b558f51da1
marceloavan <marcelo.avan@gmail.com>
2015-11-12 19:18:22 -0200

    adjustments to open shift

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+#!/bin/env node
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 var express = require('express');
 var fs = require('fs');
 
@@ -7,8 +12,10 @@ var index = fs.readFileSync('index.html');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
+  res.set('Content-Type', 'text/html');
   res.end(index);
 })
 
-app.listen(3000);
-console.log('Running in localhost 3000');
+app.listen(port, ipaddr, function() {
+  console.log('Running in ' + ipaddr + ':' + port);
+});
